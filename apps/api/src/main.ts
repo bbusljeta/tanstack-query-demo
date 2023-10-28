27d2bdb98c76e6c8c52f222d61a934ff52fae6da
Bojan Bušljeta <bojan@prototyp.digital>
2023-10-28 16:23:09 +0200

    setup versioning and openapi

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,4 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
+import { Logger, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
@@ -12,6 +7,7 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -20,8 +16,24 @@ async function bootstrap() {
   );
   const configService = app.get(ConfigService);
   const port = configService.get<number>('API_PORT');
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Template')
+    .setDescription('Template API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document: OpenAPIObject = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
-  Logger.log(`🚀 Application is running on: http://localhost:${port}/`);
+  Logger.log(`🚀 Application is running on: http://localhost:${port}/v1`);
+  Logger.log(`🚀 OpenAPI docs are available at: http://localhost:${port}/api`);
 }
 
 bootstrap();
